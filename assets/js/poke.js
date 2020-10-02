bfc35eb31ddb241fc6cf2367ae179b9523d9cb67
Glendis Pina <auraregrm@gmail.com>
2020-10-02 14:07:05 -0500

    ejercicio api

---
+++ assets/js/poke.js
@@ -0,0 +1,46 @@
+const API = " https://pokeapi.co/api/v2/pokemon";
+
+const getData = (api) => {
+    return fetch(api)
+        .then((response) => response.json())
+        .then((json) => {
+            pintar(json);
+        })
+        .catch((error) => {
+            console.log("Error :", error)
+        })
+}
+
+const pintar = (nombre) => {
+
+    nombre.results.forEach((pokemon) => {
+        pagina(pokemon.url);
+    });
+
+}
+
+const pagina = (url) => {
+
+    console.log("La url de la imagen es" + url);
+    return fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+
+            console.log(json.sprites.other.dream_world.front_default);
+            let html = "";
+            html += '<div class= "col">';
+            html += '<div class="card" style="width: 14rem; m-5">';
+            html += `<img class="card-img-top" src= "${json.sprites.other.dream_world.front_default}" alt="...">`;
+            html += '<div class="card-body">';
+            html += `<h5 class= "">${json.name}</h5>`;
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            document.getElementById("pokeData").innerHTML += html;
+
+        }).catch((error) => {
+            console.log("Error pagina :", error)
+        })
+}
+
+getData(API);
